c62d45c5b361eed3a6c9843a96bd8f3d06973fea
Vlad <vladklivak@gmail.com>
2017-10-02 10:07:01 +0300

    added ball move

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 var canvas
 var canvasContext
 
+var ballX = 75
+
 window.onload = function () {
 	canvas = document.getElementById('gameCanvas')
 	canvasContext = canvas.getContext('2d')
+
+	var framesPerSecond = 30
+	setInterval(updateAll, 1000 / framesPerSecond)
+
+}
+
+function updateAll () {
+
+	ballX++
 
 	canvasContext.fillStyle = 'black'
 	canvasContext.fillRect(0, 0, canvas.width, canvas.height)
@@ -14,3 +25,5 @@ window.onload = function () {
 	canvasContext.fill()
 
 }
+
+
